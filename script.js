cd1d60e724f2d3de1a201b4028c2e4a949a55326
Tijesuconsult <ayoolablessing54@gmail.com>
2024-05-03 16:42:25 -0700

    update

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ click_to_convert.addEventListener('click', function(){
     window.SpeechRecognition = window.webkitSpeechRecognition;
     const recognition = new SpeechRecognition();
     recognition.interimResults = true;
+
     recognition.addEventListener('result', e=>{
         const transcript =Array.from(e.results)
         .map(result => result[0])
         .map(result => result.transcript);
-        convert_text.innerHTML
+        convert_text.innerHTML = transcript;
     })
     if(speech==true){
         recognition.start();
